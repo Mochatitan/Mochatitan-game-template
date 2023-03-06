0eb5355dc9a7a123f8da275b39c05b8f9e9f5647
Mochatitan <119437575+Mochatitan@users.noreply.github.com>
2023-03-06 12:54:16 -0500

    Create Bullet.java

---
+++ src/game/Bullet.java
@@ -0,0 +1,16 @@
+public class Bullet {
+
+  private float position;
+
+  public Bullet() {
+    position = 0.0f;
+  }
+
+  public float getPosition() {
+    return position;
+  }
+
+  public void setPosition(float position) {
+    this.position = position;
+  }
+}
